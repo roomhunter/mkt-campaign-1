37c38a992ed0e70c9586a804ec1f55e10eff0c0e
to0 <yuanancai@gmail.com>
2015-05-31 22:03:37 -0700

    try encode url

--- app/scripts/configWeixin.js
+++ app/scripts/configWeixin.js
@@ -1,7 +1,9 @@
 /* jshint devel:true */
 (function configWeixin () {
   var ticketRequest = new XMLHttpRequest();
-  ticketRequest.open('GET', 'https://api.roomhunter.us/v1/ticket?url=http://marketing.roomhunter.us/where-to-live', true);
+  var url = location.href.split('#')[0];
+  alert(url);
+  ticketRequest.open('GET', 'https://api.roomhunter.us/v1/ticket?url=' + encodeURI(url), true);
   ticketRequest.responseType = 'json';
   ticketRequest.onload = function() {
     var status = this.status;
