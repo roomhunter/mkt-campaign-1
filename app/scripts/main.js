8bdb54d0602ff1b4291f9f87c2bc7aa2ac63f620
to0 <yuanancai@gmail.com>
2015-06-04 23:10:20 -0700

    upyun thumbnail not work..

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -8,7 +8,7 @@ function submitMyName() {
     title: '我在美国会住: ' + place.title + '。测测你在美国会住在哪里？', // 分享标题
     desc: place.description, // 分享描述
     link: 'http://marketing.roomhunter.us/where-to-live', // 分享链接
-    imgUrl: place.imageUrl + '!wxShareImage', // 分享图标
+    imgUrl: place.imageUrl + '!userMediumAvatar', // 分享图标
     type: 'link', // 分享类型,music、video或link，不填默认为link
     dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
     success: function () {
@@ -21,7 +21,7 @@ function submitMyName() {
   wx.onMenuShareTimeline({
     title: '我在美国会住: ' + place.title + '测测你在美国会住在哪里？', // 分享标题
     link: 'http://marketing.roomhunter.us/where-to-live', // 分享链接
-    imgUrl: place.imageUrl + '!wxShareImage', // 分享图标
+    imgUrl: place.imageUrl + '!userMediumAvatar', // 分享图标
     success: function () {
       // 用户确认分享后执行的回调函数
     },
