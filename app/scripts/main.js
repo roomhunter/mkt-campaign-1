6a060afd1351371468d6fe14e2bb2a90c1489b8b
to0 <yuanancai@gmail.com>
2015-06-04 21:44:05 -0700

    test show elements after wx ready

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,7 +22,8 @@ function getIndexOf(str, arr) {
 }
 wx.ready(function(){
   // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-
+  var game = document.getElementsByClassName('game')[0];
+  game.classList.add('ready');
   wx.onMenuShareAppMessage({
     title: '我在美国会住: ' + place.title + '测测你在美国会住在哪里？', // 分享标题
     desc: place.description, // 分享描述
