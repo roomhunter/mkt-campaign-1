7d1dd1ad638c30c252c81238446c2b0a43fc282e
to0 <yuanancai@gmail.com>
2015-06-04 21:52:32 -0700

    wrong position of wx methods

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,29 +1,9 @@
 /* jshint devel:true */
-var place;
 
 function submitMyName() {
   //e.preventDefault();
   var name = document.getElementById('name').value;
-  place = places[getIndexOf(name, places)];
-  setViewBy(place);
-  return false;
-}
-function setViewBy(place) {
-  var title = document.getElementById('place-title');
-  var description = document.getElementById('place-description');
-  title.textContent = place.title;
-  description.textContent = place.description;
-}
-function getIndexOf(str, arr) {
-  for(var ret = 0, i = 0, len = str.length; i < len; i++) {
-    ret = (31 * ret + str.charCodeAt(i)) << 0;
-  }
-  return ret % arr.length;
-}
-wx.ready(function(){
-  // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
-  var game = document.getElementsByClassName('game')[0];
-  game.classList.add('ready');
+  var place = places[getIndexOf(name, places)];
   wx.onMenuShareAppMessage({
     title: '我在美国会住: ' + place.title + '测测你在美国会住在哪里？', // 分享标题
     desc: place.description, // 分享描述
@@ -49,9 +29,24 @@ wx.ready(function(){
       // 用户取消分享后执行的回调函数
     }
   });
-});
-wx.error(function(res){
-
-  // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
+  setViewBy(place);
+  return false;
+}
+function setViewBy(place) {
+  var title = document.getElementById('place-title');
+  var description = document.getElementById('place-description');
+  title.textContent = place.title;
+  description.textContent = place.description;
+}
+function getIndexOf(str, arr) {
+  for(var ret = 0, i = 0, len = str.length; i < len; i++) {
+    ret = (31 * ret + str.charCodeAt(i)) << 0;
+  }
+  return ret % arr.length;
+}
+wx.ready(function(){
+  // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+  var game = document.getElementsByClassName('game')[0];
+  game.classList.add('ready');
 
 });
