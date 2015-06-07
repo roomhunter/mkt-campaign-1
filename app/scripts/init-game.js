f4653af26d3be8c032de729f902b7d985e4dc5c0
to0 <yuanancai@gmail.com>
2015-06-07 13:00:05 -0700

    add website bottom line

--- app/scripts/init-game.js
+++ app/scripts/init-game.js
@@ -1,5 +1,5 @@
 /* jshint devel:true */
-(function initGameResult () {
+function initGameResult () {
     var queryString = window.location.search;
     var input = document.getElementById('name');
     var submitBtn = document.getElementById('submit-btn');
@@ -11,7 +11,6 @@
         var gameInputView = document.getElementsByClassName('game-input')[0];
         gameInputView.classList.remove('offset-to-left');
         gameView.classList.remove('offset-to-left');
-        gameView.classList.add('slide-to-right');
         window.setTimeout(function (){
             gameView.classList.add('played');
         }, 1000);
@@ -37,6 +36,32 @@
             title.textContent = place.title;
             nameSpan.textContent = params.name;
             description.textContent = place.description;
+
+            wx.onMenuShareAppMessage({
+                title: params.name + '在美国会住: ' + place.title + '。测测你在美国会住在哪里？', // 分享标题
+                desc: place.description, // 分享描述
+                link: getSharableLink(index, params.name), // 分享链接
+                imgUrl: place.imageUrl + '!wxShareImage', // 分享图标
+                type: 'link', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
+            wx.onMenuShareTimeline({
+                title: params.name + '在美国会住: ' + place.title + '。测测你在美国会住在哪里？', // 分享标题
+                link: getSharableLink(index, params.name), // 分享链接
+                imgUrl: place.imageUrl + '!wxShareImage', // 分享图标
+                success: function () {
+                    // 用户确认分享后执行的回调函数
+                },
+                cancel: function () {
+                    // 用户取消分享后执行的回调函数
+                }
+            });
         }
     }
 
@@ -52,4 +77,4 @@
         }
         return params;
     }
-})();
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -46,18 +46,6 @@ function submitMyName() {
   return false;
 }
 
-function toGame() {
-  var gameView = document.getElementsByClassName('game')[0];
-  var gameInputView = document.getElementsByClassName('game-input')[0];
-  //gameInputView.classList.remove('offset-to-left');
-  //gameView.classList.remove('offset-to-left');
-  gameView.classList.remove('slide-to-left');
-  gameInputView.classList.remove('slide-to-left');
-  window.setTimeout(function (){
-    gameView.classList.add('played');
-  }, 1000);
-}
-
 // helper
 
 function setGameResultBy(place, name) {
@@ -73,10 +61,8 @@ function setGameResultBy(place, name) {
 function toResult () {
   var gameView = document.getElementsByClassName('game')[0];
   var gameInputView = document.getElementsByClassName('game-input')[0];
-  gameView.classList.remove('slide-to-right');
   gameInputView.classList.add('offset-to-left');
   gameView.classList.add('offset-to-left');
-  gameView.classList.add('slide-to-left');
 }
 function getIndexOf(str, arr) {
   for(var ret = 0, i = 0, len = str.length; i < len; i++) {
@@ -95,84 +81,5 @@ wx.ready(function(){
   var game = document.getElementsByClassName('game')[0];
   game.classList.add('ready');
 
-  (function initGameResult () {
-    var queryString = window.location.search;
-    var input = document.getElementById('name');
-    var submitBtn = document.getElementById('submit-btn');
-    var gameInputView = document.getElementsByClassName('game-input')[0];
-    var toGameBtn = document.getElementById('to-game');
-
-    toGameBtn.onclick = function () {
-      var gameView = document.getElementsByClassName('game')[0];
-      var gameInputView = document.getElementsByClassName('game-input')[0];
-      gameInputView.classList.remove('offset-to-left');
-      gameView.classList.remove('offset-to-left');
-      gameView.classList.add('slide-to-right');
-      window.setTimeout(function (){
-        gameView.classList.add('played');
-      }, 1000);
-    };
-    input.oninput = function () {
-      if (input.value.length > 0) {
-        submitBtn.classList.remove('disabled');
-      }
-      else {
-        submitBtn.classList.add('disabled');
-      }
-    };
-    if (queryString) {
-      var params = parseQueryString(decodeURIComponent(queryString.substring(1)));
-      var title = document.getElementById('place-title');
-      var description = document.getElementById('place-description');
-      var image = document.getElementById('place-image');
-      var nameSpan = document.getElementById('name-title');
-      var index = params.placeIndex || 0;
-      var place = places[index] || places[0];
-      if (params.name && place) {
-        image.setAttribute('src', place.imageUrl);
-        title.textContent = place.title;
-        nameSpan.textContent = params.name;
-        description.textContent = place.description;
-
-        wx.onMenuShareAppMessage({
-          title: params.name + '在美国会住: ' + place.title + '。测测你在美国会住在哪里？', // 分享标题
-          desc: place.description, // 分享描述
-          link: getSharableLink(index, params.name), // 分享链接
-          imgUrl: place.imageUrl + '!wxShareImage', // 分享图标
-          type: 'link', // 分享类型,music、video或link，不填默认为link
-          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-          success: function () {
-            // 用户确认分享后执行的回调函数
-          },
-          cancel: function () {
-            // 用户取消分享后执行的回调函数
-          }
-        });
-        wx.onMenuShareTimeline({
-          title: params.name + '在美国会住: ' + place.title + '。测测你在美国会住在哪里？', // 分享标题
-          link: getSharableLink(index, params.name), // 分享链接
-          imgUrl: place.imageUrl + '!wxShareImage', // 分享图标
-          success: function () {
-            // 用户确认分享后执行的回调函数
-          },
-          cancel: function () {
-            // 用户取消分享后执行的回调函数
-          }
-        });
-      }
-    }
-
-    function parseQueryString(queryString) {
-      var params = {}, queries, temp, i, l;
-      // Split into key/value pairs
-      queries = queryString.split("&");
-
-      // Convert the array of strings into an object
-      for ( i = 0, l = queries.length; i < l; i++ ) {
-        temp = queries[i].split('=');
-        params[temp[0]] = temp[1];
-      }
-      return params;
-    }
-  })();
+  initGameResult();
 });
